ac7b05dfae1a2847ea4f5cfce3db5d0330735fe4
tuan.le2 <tuan.le2@sotatek.com>
2024-02-02 14:32:53 +0700

    fix: prevent opening context menu

--- src/components/Navigator.tsx
+++ src/components/Navigator.tsx
@@ -8,12 +8,17 @@ const Navigator: React.FC<INavigatorProps> = (props) => {
   const isLeft = type === ENavigatorType.Left;
   const className = `navigator navigator--${isLeft ? "left" : "right"}`;
 
+  const _preventOpenContextMenu = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    event.preventDefault();
+  };
+
   return (
     <button
       className={className}
       onPointerDown={onMouseDown}
       onPointerUp={onMouseUp}
       onPointerLeave={onMouseLeave}
+      onContextMenu={_preventOpenContextMenu}
     >
       <img src="./assets/icons/EyesIcon.png" alt="Eye icon" />
     </button>
